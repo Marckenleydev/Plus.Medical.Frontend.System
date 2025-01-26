61de29ade4c41e55a3fe33ece31c0204d6384dcc
Marckenley Dorsainvil <marckenlygbeau@gmail.com>
2025-01-26 21:50:18 +0400

    code updating Change Backend Url Api

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,9 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  
 })
